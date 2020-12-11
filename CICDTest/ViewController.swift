38a30f54b72e769e002eb249bba2a7981e60c399
BlackChen <blackchen@best-developer.cn>
2020-12-11 13:06:08 +0800

    测试1

--- CICDTest/ViewController.swift
+++ CICDTest/ViewController.swift
@@ -12,8 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let btn = UIButton.init(frame: .init(x: 100, y: 100, width: 100, height: 30))
+        btn.backgroundColor = .brown
+        btn.setTitle("CICD", for: .normal)
+        btn.addTarget(self, action: .init(("touched")), for: .touchUpInside)
+        
+        
     }
 
+    func touched (){
+        let alert = UIAlertController.init(title: "CICD", message: "持续集成测试", preferredStyle: .alert)
+        alert.addAction(.init(title: "OK", style: .cancel, handler: { _ in
+            
+        }))
+        
+        self.present(alert, animated: true) {
+            
+        }
+        
+    }
 
 }
 
